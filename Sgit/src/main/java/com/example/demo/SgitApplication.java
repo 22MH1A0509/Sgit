e8cf138cdde24d8c939ffd18178e8c7db66b12ec
VANDANA <22mh1a0509@acoe.edu.in>
2025-09-20 19:04:13 +0530

    this is my first commit

---
+++ Sgit/src/main/java/com/example/demo/SgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgitApplication.class, args);
+	}
+
+}
